d2118f1b938630aaa7747961d758c7ac40a43cac
gabriel <gabrielimabelo@gmail.com>
2024-04-01 23:10:58 +0100

    module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post }  from "./Post";
+import { Header } from "./components/Header";
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Gabriel Beli"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, nobis aliquid perferendis fugiat voluptatibus alias esse obcaecati doloremque dolore soluta autem ipsum, corrupti illo delectus ipsam doloribus voluptatum? Minus, officiis!" 
