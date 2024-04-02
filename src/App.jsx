05bdf086cc54fe238e9f9aab8787558da1257817
gabriel <gabrielimabelo@gmail.com>
2024-04-02 01:26:00 +0100

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Post }  from "./Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -7,14 +10,20 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Gabriel Beli"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, nobis aliquid perferendis fugiat voluptatibus alias esse obcaecati doloremque dolore soluta autem ipsum, corrupti illo delectus ipsam doloribus voluptatum? Minus, officiis!" 
-      />
-      <Post
-        author="Joana Dark"
-        content="O fogo não é nada, se não, a propria raiva" 
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+          author="Gabriel Beli"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, nobis aliquid perferendis fugiat voluptatibus alias esse obcaecati doloremque dolore soluta autem ipsum, corrupti illo delectus ipsam doloribus voluptatum? Minus, officiis!" 
+          />
+          <Post
+          author="Joana Dark"
+          content="O fogo não é nada, se não, a propria raiva" 
+          />
+        </main>
+      </div>
     </div>
   )
 }
